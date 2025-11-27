fac6a756274928a3387c253bb53c9de6c0576003
Tấn Phát <ryanpham0226@gmail.com>
2025-11-27 20:54:16 +0700

    feat: initialize backend server with Express and environment configuration
    - Added package.json for backend dependencies and scripts.
    - Created server.js to set up the Express server and handle graceful shutdown.
    - Implemented app.js to configure middleware for JSON parsing.
    - Added environment.js to manage environment variables using dotenv.

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import app from './src/app.js'
+import { env } from './src/configs/envỉonment.js'
+
+// Listen server
+const server = app.listen(env.PORT, () => {
+	console.log(`Server is running on port ${env.PORT}`)
+})
+
+/**
+ * Gracefully shuts down the server on SIGINT signal (e.g., Ctrl+C).
+ * It stops the server from accepting new connections and waits for
+ * existing connections to close before exiting.
+ */
+process.on('SIGINT', () => {
+	server.close(() => {
+		console.log('Exit Server Express')
+	})
+})
